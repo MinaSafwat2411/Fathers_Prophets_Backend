31cfe08ad50169b13c6639bc0b4c487e5bce3c49
Mina Safwat <189299988+MinaSafwat2411@users.noreply.github.com>
2026-05-24 17:20:59 +0300

    Add database tables for prophets, fathers, and saints with relevant fields

---
+++ src/main/resources/db/migration/V11_create_fathers_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS fathers
+(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    title       VARCHAR(255),
+    short_story VARCHAR(255),
+    full_story  VARCHAR(255),
+    image       VARCHAR(255)
+);
